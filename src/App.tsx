aedbbcd1c7fda93d8748ae563ea57482d11cac9f
JashanDhiman <jashandhiman@smartdatainc.net>
2023-07-04 18:31:38 +0530

    saving timeline and use that after wards

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useLayoutEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import "./App.css";
 import { gsap } from "gsap";
 
 function App() {
- // const [count, setCount] = useState(0);
+ const [toggle, setToggle] = useState(false);
  const app = useRef<HTMLDivElement>(null);
  const tl = useRef<HTMLDivElement>(null);
  // gsap.to(square, { rotate: 360, duration: 3 });
@@ -14,23 +14,43 @@ function App() {
  };
  useLayoutEffect(() => {
   let ctx = gsap.context(() => {
-   tl.current = gsap.timeline().to(".square", { rotate: 360, duration: 2 }).to(".square2", { x: 200 });
+   tl.current = gsap.timeline().to(".square", { rotate: 360, yoyo: true, duration: 2 }).to(".square2", { yoyo: true, x: 200 });
+   //   gsap.to(".square", {
+   //    rotation: 360,
+   //    repeat: -1,
+   //    repeatDelay: 1,
+   //    yoyo: true,
+   //   });
+
+   //   gsap.to(".square2", {
+   //    x: 100,
+   //    repeat: -1,
+   //    repeatDelay: 1,
+   //    yoyo: true,
+   //   });
   }, app);
   return () => ctx.revert();
  }, []);
- const onEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  gsap.to(e.target, { scale: 1.2 });
- };
- const onLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  gsap.to(e.target, { scale: 1 });
- };
- const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  gsap.to(e.target, { rotation: "50", yoyo: true, repeat: 1 });
- };
+
+ useEffect(() => {
+  tl.current.reversed(toggle);
+ }, [toggle]);
+
+ // const onEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+ //  gsap.to(e.target, { scale: 1.2 });
+ // };
+ // const onLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+ //  gsap.to(e.target, { scale: 1 });
+ // };
+ // const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+ //  gsap.to(e.target, { rotation: "50", yoyo: true, repeat: 1 });
+ // };
+
  return (
-  <>w
+  <>
    <div ref={app}>
-    <div style={squareStyle} className="square" onMouseEnter={onEnter} onMouseLeave={onLeave} onClick={handleClick}>
+    <button onClick={() => setToggle(!toggle)}>Toggle</button>
+    <div style={squareStyle} className="square">
      Hello World
     </div>
     <div style={squareStyle} className="square2">
